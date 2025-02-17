c7d2daba44aceb79042f7dcf6a1c177b31ebc288
Kishore2268 <kishore@anivarti.in>
2025-02-17 17:41:05 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://clothing-ecommerc-store.onrender.com",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
