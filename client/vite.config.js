e4b6ffc94c212128c6611d63f319285ab47bbe71
Kishore2268 <kishore@anivarti.in>
2025-02-17 17:26:22 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,24 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "build", // Set build output directory to "build"
+    chunkSizeWarningLimit: 500, // Adjust chunk size warning limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Moves dependencies to a separate chunk
+          }
+        },
+      },
     },
   },
 });
